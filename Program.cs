a2d62afedefe1aa64373f119a5997e284a308029
muradalakbarli <146637971+muradalakbarli@users.noreply.github.com>
2023-10-18 22:57:53 +0400

    Task 2 completed

--- Program.cs
+++ Program.cs
@@ -25,8 +25,75 @@
             Console.WriteLine("nezere alinmayib");
         }
 
-
-
     }
 
 }
+
+
+
+
+class Task2
+{
+    static void Main()
+    {
+
+        int qh = 5; // qapi
+        int qe = 2;
+
+        int sh = 4; // skaf
+        int se = 5;
+        int su = 2;
+
+        bool qapidankecir = false;
+
+        if (sh <= qh && se <= qe)
+
+        {
+            qapidankecir = true;
+        }
+
+        else if (su <= qh && se <= qe)
+
+        {
+            qapidankecir = true;
+        }
+
+        else if (sh <= qh && su <= qe)
+
+        {
+            qapidankecir = true;
+        }
+
+        else if (sh <= qe && se <= qh)
+
+        {
+            qapidankecir = true;
+        }
+
+        else if (su <= qe && se <= qh)
+
+        {
+            qapidankecir = true;
+        }
+
+        else if (sh <= qe && se <= qh)
+
+        {
+            qapidankecir = true;
+        }
+
+        if (qapidankecir)
+
+        {
+            Console.WriteLine("skaf qapidan kecir");
+        }
+
+        else
+
+        {
+            Console.WriteLine("skaf qapidan kecmir");
+        }
+
+    }
+}
+
