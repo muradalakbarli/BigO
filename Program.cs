71f7ce268be9c9b968f3e6babadff9a112e77ecb
Elcın <elcin@Murad-2.local>
2023-10-18 11:48:39 +0400

    Task completed

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿class Task1
+{
+    static void Main()
+    {
+        int n = 1090;
+        int m = 4;
+
+        int sum = n + m;
+
+        if (sum >= 10 && sum <= 99)
+
+        {
+            Console.WriteLine("cem ikireqemli ededdir");
+        }
+
+        if (sum >= 100 && sum <= 999)
+
+        {
+            Console.WriteLine("cem uc reqemli ededdir");
+        }
+
+        if (sum >= 1000 || sum <= 9)
+
+        {
+            Console.WriteLine("nezere alinmayib");
+        }
+
+
+
+    }
+
+}
